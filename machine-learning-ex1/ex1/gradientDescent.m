a430c9bfbfa02b9702e35b9f99a77396ef9a1683
abdelrahman fekri <abdelrahman.fekri@bld.ai>
2022-12-23 21:39:28 +0200

    machine learning ex

---
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,34 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    sumi=zeros(length(theta),1);
+    for j=1:length(theta)
+     for i=1:m
+       sumi(j)=sumi(j)+(X(i,:)*theta-y(i))*X(i,j);
+       i=i+1;
+     end
+    j=j+1;
+    end
+    for j=1:length(theta)
+       theta(j)=theta(j)-alpha/m*sumi(j);
+       j=j+1;
+    end
+    J_history(iter)=J_history(iter)+computeCost(X,y,theta);
+    iter=iter+1;
+end
+
+end
